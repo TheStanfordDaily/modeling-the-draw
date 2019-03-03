0abfabfd2d5a9ec36d9f03c6ea20ed60bd1eb350
lilymzhou <lilyzhou@stanford.edu>
2019-03-03 13:18:31 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+                      </p>
           <a
             className="App-link"
             href="https://reactjs.org"
