508b2bf5d3fff393c82c8617b213f51a2617a3e6
lilymzhou <lilyzhou@stanford.edu>
2019-03-29 16:15:44 -0700

    read json files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
-//import Flexbox from 'flexbox-react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-//import Button from 'react-bootstrap/Button';
-import './App.css';
-import Form from "react-jsonschema-form";
-//import CsvParse from '@vtex/react-csv-parse';
+/*
+import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import data from "csv-loader!./housingData1718_cleaned.csv";
+*/
+import './App.css';
+import Form from "react-jsonschema-form";
 
 const schema = {
   title: "Calculator",
@@ -65,26 +61,45 @@ function findLeastSquares(x_values, y_values) {
 }
 
 
-function processTrends(h_data, gender, typeCol, resID, des_year, simple, linear, logistic) {
-  let cutoffs = [];
-  let yearList = [2018]; //should actually be "let yearList = [2015, 2016, 2018]"
+function processTrends(gender, typeCol, resID, des_year, simple, linear, logistic) {
+  const data_1718 = require('./housingData1718.json');
+  const data_16 = require('./housingData16.json');
+  const data_15 = require('./housingData15.json');
 
-  let i;
-  for (i = 0; i < yearList.length; i++) {
-    let h_data_yearSlice = h_data["year" == yearList[i]];
-    
-    /*
-    let h_data_genderSlice;
-    if (gender != "n") {
-      h_data_genderSlice = h_data_yearSlice["sex" == gender];
-      const cutoff_i = h_data_genderSlice["res_name_edited" == resID]; //assuming that there is only one item matching this
-      cutoffs.push(h_data_genderSlice[cutoff_i][typeCol]);
-    } else {
-      h_data_genderSlice = h_data_yearSlice;
-      const cutoff_is = h_data_genderSlice["res_name_edited" == resID];
-      cutoffs.push((h_data_genderSlice[cutoff_is[0]][typeCol] + h_data_genderSlice[cutoff_is[1]][typeCol]) / 2);
+  let cutoffs = [];
+  let yearList = [2015, 2016, 2017, 2018];
+
+  let currData;
+  for (let i = 0; i < yearList.length; i++) {
+    switch (yearList[i]) {
+      case 2015:
+        currData = data_15;
+        break;
+      case 2016:
+        currData = data_16;
+        break;
+      case 2017 || 2018:
+        currData = data_1718;
     }
-    */
+    for (let j = 0; j < currData.length; j++) {
+      let item = currData[j];
+      if (item.year == yearList[i] && (gender == "n" || item.sex == gender) && item.res_name_edited == resID) {
+        switch (typeCol) {
+          case "individual":
+            cutoffs.push(item.individual);
+            break;
+          case "group_2":
+            cutoffs.push(item.group_2);
+            break;
+          case "group_3":
+            cutoffs.push(item.group_3);
+            break;
+          case "group_4":
+            cutoffs.push(item.group_4);
+        }
+        break;
+      }
+    }
   }
 
   if (cutoffs.length == 1) {
@@ -96,80 +111,73 @@ function processTrends(h_data, gender, typeCol, resID, des_year, simple, linear,
   return 0;
 }
 
-/* sex, roomtype, residence, tiernumber, applytype */
-function processSingleQuery(sex, roomtype, residence, tiernumber, applytype) {
-  const gender_raw = sex;
-  const roomType_raw = roomtype;
-  const resName_raw = residence;
-  const tierNum_raw = tiernumber;
-  const applyType_raw = applytype;
+function processSingleQuery(gender_raw, roomType_raw, resName_raw, tierNum_raw, applyType_raw) {
 
   /* gender */
   let gender;
   switch(gender_raw) {
-    case 'male' :
-      gender = 'm';
+    case "male":
+      gender = "m";
       break;
-    case 'female':
-      gender = 'f';
+    case "female":
+      gender = "f";
       break;
     default:
-      gender = 'n';
+      gender = "n";
   }
 
   /* room type + residence */
   let roomType;
-
   switch(roomType_raw) {
-    case 'a 1 room single':
-      roomType = '1 Room Single';
+    case "a 1 room single":
+      roomType = "1 Room Single";
       break;
-    case 'a 1 room double':
-      roomType = '1 Room Double';
+    case "a 1 room double":
+      roomType = "1 Room Double";
       break;
-    case 'a 1 room double (focus)' :
-      roomType = '1 Room Double (focus)';
+    case "a 1 room double (focus)" :
+      roomType = "1 Room Double (focus)";
       break;
-    case 'a 2 room double' :
-      roomType = '2 Room Double';
+    case "a 2 room double" :
+      roomType = "2 Room Double";
       break;
-    case 'a 2 room double (focus)' : 
-      roomType = '2 Room Double (focus)';
+    case "a 2 room double (focus)" : 
+      roomType = "2 Room Double (focus)";
       break;
-    case 'a triple ' : 
-      roomType = 'Triple';
+    case "a triple" : 
+      roomType = "Triple";
       break;
-    case 'a standard room ' : 
-      roomType = 'Standard';
+    case "a standard room" : 
+      roomType = "Standard";
       break;
-    case 'a premium room ' :
-      roomType = 'Premium';
+    case "a premium room" :
+      roomType = "Premium";
       break;
-    case 'substance free housing ' :
-      roomType = 'Substance Free Housing';
+    case "substance free housing" :
+      roomType = "Substance Free Housing";
       break;
-    case 'ethnic housing ' :
-      roomType = 'ETHNIC';
+    case "ethnic housing" :
+      roomType = "ETHNIC";
       break;
     default:
-      roomType = 'Any';
+      roomType = "Any";
   }
   if (roomType == "ETHNIC") {
     switch (resName_raw) {
-      case 'Ujamaa':
-        roomType = 'Ethnic' + 'B';
+      case "Ujamaa":
+        roomType = "Ethnic" + "B";
         break;
-      case 'Hammarskjold':
-        roomType = 'Ethnic' + 'I';
+      case "Hammarskjold":
+        roomType = "Ethnic" + "I";
         break;
-      case 'Muwekma':
-        roomType = 'Ethnic' + 'N';
+      case "Muwekma":
+        roomType = "Ethnic" + "N";
         break;
-      case 'Zapata':
-        roomType = 'Ethnic' + 'C';
+      case "Zapata":
+        roomType = "Ethnic" + "C";
         break;
-      case 'Okada':
-        roomType = 'Ethnic' + 'A';
+      case "Okada":
+        roomType = "Ethnic" + "A";
         break;
     }
   }
@@ -178,37 +186,36 @@ function processSingleQuery(sex, roomtype, residence, tiernumber, applytype) {
   /* applytype (number of ppl in group) */
   let typeCol;
   switch (applyType_raw) {
-    case 'an individual':
-      typeCol = 'individual';
+    case "an individual" :
+      typeCol = "individual";
       break;
-    case 'a group of 2':
-      typeCol = 'group_2';
+    case "a group of 2" :
+      typeCol = "group_2";
       break;
-    case 'a group of 3':
-      typeCol = 'group_3';
+    case "a group of 3" :
+      typeCol = "group_3";
       break;
-    case 'a group of 4':
-      typeCol = 'group_4';
+    case "a group of 4" :
+      typeCol = "group_4";
       break;
   }
 
   /* tier number */
   const tierNum = tierNum_raw;
-  
-  let output = '';
 
+  let output = '';
   /* find percentage */
   const score_ceiling = tierNum * 1000;
   const score_floor = score_ceiling - 999;
 
-  const cutoff = processTrends(data, gender, typeCol, resID, 2019, false, true, false);
+  const cutoff = processTrends(gender, typeCol, resID, 2019, false, true, false);
 
   if (cutoff > score_ceiling) {
     output = '>99';
   } else if (cutoff < score_floor) {
     output = '<0.1';
   } else {
-    output = (cutoff - score_floor) / 1000;
+    output = (cutoff - score_floor) / 10;
   }
 
   return output;
@@ -227,40 +234,25 @@ class App extends React.Component {
   }
 
   onSubmit = ({formData}) => {
-    const sex = JSON.stringify(formData.sex, null, 2);
-    const roomtype = JSON.stringify(formData.roomtype, null, 2);
-    const residence = JSON.stringify(formData.residence, null, 2);
-    const tiernumber = JSON.stringify(formData.tiernumber, null, 2);
-    const applytype = JSON.stringify(formData.applytype, null, 2);
+    let sex = formData.sex;
+    let roomtype = formData.roomtype;
+    let residence = formData.residence;
+    let tiernumber = formData.tiernumber;
+    let applytype = formData.applytype;
 
     this.setState({results: processSingleQuery(sex, roomtype, residence, tiernumber, applytype)});
   }
-
-  /*
-  handleData = data => {
-    this.setState({data})
-  }
-  */
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
-          <Container>
-            <Row>
-              <Col>
-                <Form schema={schema}
-                onChange={log("changed")}
-                onSubmit={this.onSubmit}
-                formData={this.formData}
-                onError={onError} />
-              </Col>
-              <Col>
-                <div>{this.state.results && `Your Chances: ${this.state.results}` }</div> 
-              </Col>
-            </Row>
-          </Container>
+          <Form schema={schema}
+            onChange={log("changed")}
+            onSubmit={this.onSubmit}
+            formData={this.formData}
+            onError={onError} />
+          <div>{this.state.results && `Your Chances: ${this.state.results}%` }</div> 
         </header>
       </div>
     );
