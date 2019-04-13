1ea3c0eb0e78d2095035aaf44592043a9a5df198
lilymzhou <lilyzhou@stanford.edu>
2019-04-13 13:08:55 -0700

    Updates

---
+++ precache-manifest.22b53ebd3403ce1437ec8fd00053b453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7d474ce9924176589a",
+    "url": "/modeling-the-draw/static/js/runtime~main.af7d474c.js"
+  },
+  {
+    "revision": "76a786108400c75a6441",
+    "url": "/modeling-the-draw/static/js/main.76a78610.chunk.js"
+  },
+  {
+    "revision": "bcae935bca9fe23d5945",
+    "url": "/modeling-the-draw/static/js/2.bcae935b.chunk.js"
+  },
+  {
+    "revision": "76a786108400c75a6441",
+    "url": "/modeling-the-draw/static/css/main.28c508a5.chunk.css"
+  },
+  {
+    "revision": "ca99ae241959718ed0a1e86f89f362cc",
+    "url": "/modeling-the-draw/index.html"
+  }
+];
