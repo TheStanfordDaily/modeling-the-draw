95b851912e7350bf956da8d22e3e1a12744064df
lilymzhou <lilyzhou@stanford.edu>
2019-04-19 13:43:18 -0700

    Updates

---
+++ precache-manifest.7ca51cb09aa1649553a10751300f73ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7d474ce9924176589a",
+    "url": "/modeling-the-draw/static/js/runtime~main.af7d474c.js"
+  },
+  {
+    "revision": "a37ae63992a65b6b9b6a",
+    "url": "/modeling-the-draw/static/js/main.a37ae639.chunk.js"
+  },
+  {
+    "revision": "caca5640232c1d1718f3",
+    "url": "/modeling-the-draw/static/js/2.caca5640.chunk.js"
+  },
+  {
+    "revision": "a37ae63992a65b6b9b6a",
+    "url": "/modeling-the-draw/static/css/main.75ea7f27.chunk.css"
+  },
+  {
+    "revision": "7f1e361a074e2a1809eaeaef5e5b8acc",
+    "url": "/modeling-the-draw/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/modeling-the-draw/precache-manifest.5c88eca34b60b4f231a32af8b16f3379.js"
+  "/modeling-the-draw/precache-manifest.7ca51cb09aa1649553a10751300f73ac.js"
 );
 
 workbox.clientsClaim();
